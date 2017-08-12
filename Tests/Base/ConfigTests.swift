92ed103d6c664929bfc6b88b274f6835ddd6a029
Moritz Lang <moritz@slashkeys.com>
2017-08-13 00:21:12 +0200

    [test] the ability to ask for permissions

--- Tests/Base/ConfigTests.swift
+++ Tests/Base/ConfigTests.swift
@@ -38,4 +38,21 @@ final class ConfigTests: XCTestCase {
     let config = FacebookAuth.Config(appID: appID)
     XCTAssertEqual(config.oAuthURL(permissions: [.publicProfile,.adsRead,.userAboutMe]), expectedURL)
   }
+
+  func test_it_returns_the_correct_oauth_url_to_ask_for_permissions() {
+    let appID = "43145761437"
+    let queryItems = [
+      URLQueryItem(name: "client_id", value: appID),
+      URLQueryItem(name: "redirect_uri", value: "fb\(appID)://authorize"),
+      URLQueryItem(name: "response_type", value: "token,granted_scopes"),
+      URLQueryItem(name: "scope", value: "public_profile,ads_read,user_about_me"),
+      URLQueryItem(name: "auth_type", value: "rerequest")
+    ]
+    var urlComponents = URLComponents(string: "https://www.facebook.com/v2.10/dialog/oauth")!
+    urlComponents.queryItems = queryItems
+    let expectedURL = urlComponents.url!
+
+    let config = FacebookAuth.Config(appID: appID)
+    XCTAssertEqual(config.oAuthURL(permissions: [.publicProfile,.adsRead,.userAboutMe], rerequest: true), expectedURL)
+  }
 }
